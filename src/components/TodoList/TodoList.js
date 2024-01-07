10a77f3c5a16881a12722133249bc58e929f141a
Gaurav Modi <Gauravmodi2003@gmail.com>
2024-01-07 10:57:08 -0800

    Started Todo list project

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import TodoItem from './TodoItem/TodoItem';
+
+function TodoList() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "Doctor's appointment",
+            completed: true
+        },
+        {
+            id: 2,
+            text: "Meeting at School",
+            completed: false
+        }
+    ]);
+
+    const [text, setText] = useState('');
+    
+    function addTask(text) {
+        const newTask = {
+            id: Date.now(),
+            text,
+            completed: false
+        };
+
+        setTasks([...tasks, newTask]);
+        setText('');
+    }
+
+    function deleteTask(id) {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
+    function toggleCompleted(id) {
+        setTasks(tasks.map(task => {
+            if (task.id === id) {
+                return {...task, completed: !task.completed};   // reverse the completion status
+            } else {
+                return task;
+            }
+        }));
+    }
+    
+    return (
+        <div className="TodoList">
+            {tasks.map(task => (
+                <TodoItem
+                    KEY = {task.id}
+                    TASK = {task}
+                    DELETETASK = {deleteTask}
+                    TOGGLECOMPLETED = {toggleCompleted}
+                />
+            ))}
+            <input 
+                value={text}
+                onChange={E=> setText(E.target.value)}
+            />
+            <button
+                onClick={(text) => addTask(text)}>
+                Add
+            </button>
+        </div>
+    );
+}
+
+export default TodoList;
