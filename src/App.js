85bc3b0ce7bd10f9a6a4c26255147812a40c1ac6
Gaurav Modi <Gauravmodi2003@gmail.com>
2024-01-10 20:48:20 -0800

    Added basic node.js back-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TodoList from './components/TodoList/TodoList';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+
   return (
     <div className="App">
+      <h1>{message}</h1>
       <TodoList/>
     </div>
   );
