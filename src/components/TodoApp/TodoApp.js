e5f80a7070ffc9450426a704fb448da8d8f7ec8d
Gaurav Modi <Gauravmodi2003@gmail.com>
2024-03-05 03:03:29 -0800

    Added capability of obtaining specific list from database

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList/TodoList';
+import { Card } from 'react-bootstrap';
 
 const TodoApp = (props) => {
+
+    const [listTitles, setListTitles] = useState([]);
 
     useEffect(() => {
         // Function to be called immediately upon component mount
@@ -10,26 +13,49 @@ const TodoApp = (props) => {
 
     const token = props.token;
 
+    const openList = async (task) => {
+        const list_id = task.list_id;
+        console.log('openList: ' + list_id);
+        try {
+            const response = await fetch('http://localhost:8009/list', {
+                method: 'POST',
+                headers: { 
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ list_id }),
+            });
+
+            if (response.ok) { // in the 200 range
+                const data = await response.json();
+                console.log(data.tasks);
+            } else {
+                const errorData = await response.json();
+                console.log(errorData);
+            }
+
+        } catch (error) {
+            console.error('Error: ', error);
+        };
+    };
+
     const getTodoList = async () => {
         try {
             const response = await fetch('http://localhost:8009/todo', {
                 method: 'POST',
                 headers: {
-                    'Authorization': 'Bearer ' + props.token,
+                    'Authorization': 'Bearer ' + token,
                     'Content-Type': 'application/json',
                 },
-                // body: JSON.stringify({ }),
             });
 
-            if (response.ok) { // in the 200 range?
+            if (response.ok) { // in the 200 range
                 const data = await response.json();
-                const title = data.lists;
-                console.log(title);
-                
+                setListTitles(data.lists);
+
             } else {
                 const errorData = await response.json();
-
-
+                console.log(errorData);
             }
         } catch (error) {
             console.error('Error: ', error);
@@ -39,14 +65,25 @@ const TodoApp = (props) => {
     
 
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <div className="card-body">
-                <h5 className="card-title">{props.token}</h5>
-                {/* <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
+        // <div className="card" style={{width: "18rem"}}>
+        //     <div className="card-body">
+        //         <h5 className="card-title">{props.token}</h5>
+        //         {/* <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
+        //         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        //         <a href="#" className="card-link">Card link</a>
+        //         <a href="#" className="card-link">Another link</a>
+        //     </div>
+        // </div>
+
+        <div>
+            {listTitles.map(task => (
+                <div className="card shadow-sm p-2 mb-3 .bg-light.bg-gradient rounded" 
+                    onClick={() => openList(task)} >
+                    <div className="card-title">
+                        {task.title}
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
